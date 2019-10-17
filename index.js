67bd65c0936d4bdd7e98bc72a6fba45491e286bb
David Pfahler <david@excellenteasy.com>
2019-10-17 17:11:25 +0200

    feat: download file test

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import * as RNFS from 'react-native-fs';
+
+;(async () => {
+    const { jobid, promise } = RNFS.downloadFile({
+        fromUrl: "https://picsum.photos/200",
+        toFile: RNFS.DocumentDirectoryPath + '/photo.jpg',
+    });
+    const res = await promise;
+    console.log(res);
+})();
 
 AppRegistry.registerComponent(appName, () => App);
