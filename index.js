d492eb66bf1b91251d89d3a4c512cf7cdb3ffec6
David Pfahler <david@excellenteasy.com>
2019-10-17 17:52:06 +0200

    feat: load file from bundle

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import * as RNFS from 'react-native-fs';
 
-;(async () => {
-    const { jobid, promise } = RNFS.downloadFile({
-        fromUrl: "https://picsum.photos/200",
-        toFile: RNFS.DocumentDirectoryPath + '/photo.jpg',
-    });
-    const res = await promise;
-    console.log(res);
-})();
+import resolveAssetSource from "react-native/Libraries/Image/resolveAssetSource";
+import modelPath from './dogs-resnet18.mlmodel';
+
+setTimeout(async () => {
+    try {
+        const resource = await resolveAssetSource(modelPath);
+        const blob = await fetch(resource.uri);
+        console.log("myResource was loaded: ", blob);
+    } catch (e) {
+        console.log("error", e)
+    }
+}, 500)
 
 AppRegistry.registerComponent(appName, () => App);
