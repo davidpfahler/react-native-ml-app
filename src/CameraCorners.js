7296ac91dc5a8874b9530a7961aa493d994e4f3d
David Pfahler <david@excellenteasy.com>
2019-10-30 12:10:13 +0100

    feat: flip camera

--- src/CameraCorners.js
+++ src/CameraCorners.js
@@ -1,45 +1,48 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React from 'react';
+import styled from 'styled-components/native';
 
-const WIDTH = '4px'
+import colors from './colors';
+
+const WIDTH = '4px';
 
 const Corners = styled.View`
     position: absolute;
     padding-top: 100%;
     width: 100%;
-`
+`;
 const Base = styled.View`
     position: absolute;
     height: 60px;
     width: 60px;
     border-color: ${({ color }) => color};
-`
+    margin: 2px;
+`;
 const TopLeft = styled(Base)`
     top: ${({ padding }) => padding};
     left: ${({ padding }) => padding};
     border-left-width: ${WIDTH};
     border-top-width: ${WIDTH};
-`
+`;
 const TopRight = styled(Base)`
     top: ${({ padding }) => padding};
     right: ${({ padding }) => padding};
     border-right-width: ${WIDTH};
     border-top-width: ${WIDTH};
-`
+`;
 const BottomLeft = styled(Base)`
     bottom: ${({ padding }) => padding};
     left: ${({ padding }) => padding};
     border-left-width: ${WIDTH};
     border-bottom-width: ${WIDTH};
-`
+`;
 const BottomRight = styled(Base)`
     bottom: ${({ padding }) => padding};
     right: ${({ padding }) => padding};
     border-right-width: ${WIDTH};
     border-bottom-width: ${WIDTH};
-`
+`;
 
-export default ({ color = '#3b4a4f', height, padding = 0 }) => {
+export default ({ color = colors.secondary, height, padding = 0 }) => {
     return (
         <Corners pointerEvents="none">
             <TopLeft color={color} padding={padding} />
@@ -47,5 +50,5 @@ export default ({ color = '#3b4a4f', height, padding = 0 }) => {
             <BottomLeft color={color} padding={padding} />
             <BottomRight color={color} padding={padding} />
         </Corners>
-    )
+    );
 }
--- src/CameraScreen.js
+++ src/CameraScreen.js
@@ -1,17 +1,40 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components/native';
 import { RNCamera } from 'react-native-camera';
 
 import CameraCorners from './CameraCorners';
+import colors from './colors';
 
 const Flex = styled.View`
-  flex: ${({flex = 1}) => flex};
+  flex: ${({ flex = 1 }) => flex};
 `
 
-const colors = {
-  primary: 'transparent',
-  secondary: 'white',
-};
+const Header = styled.View`
+    margin-top: 30;
+    height: 72px;
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-end;
+`
+
+const FlipButton = styled.TouchableOpacity`
+    flex: 0.3;
+    height: 40;
+    margin-horizontal: 2;
+    margin-bottom: 10;
+    margin-top: 20;
+    border-radius: 8;
+    border-color: ${colors.secondary};
+    border-width: 1;
+    padding: 5px;
+    align-items: center;
+    justify-content: center;
+`
+
+const FlipText = styled.Text`
+    font-size: 15;
+    color: ${colors.secondary};
+`
 
 const Content = styled.View`
   position: absolute;
@@ -30,7 +53,7 @@ const Fill = styled.View`
 
 const TriggerWrap = styled.View`
   width: 70px;
-  margin-bottom: ${({inset = 0}) => inset + 12};
+  margin-bottom: ${({ inset = 0 }) => inset + 12};
   height: 70px;
   border: 5px;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
@@ -49,39 +72,46 @@ const Trigger = styled.TouchableOpacity`
 `
 
 const captureOpts = {
-  width: 299,
-  height: 299,
-  quality: 1.0,
-  base64: false,
-  pauseAfterCapture: false,
-  exif: false,
+    width: 299,
+    height: 299,
+    quality: 1.0,
+    base64: false,
+    pauseAfterCapture: false,
+    exif: false,
 };
 
-export default function CameraScreen ({onPicTaken, safeAreaInsets}) {
-  const camera = useRef(null);
-  const takePictureAsync = async () => {
-    if (!camera || !camera.current) {
-      throw new Error("camera not referenced yet");
+export default function CameraScreen({ onPicTaken, safeAreaInsets }) {
+    const camera = useRef(null);
+    const takePictureAsync = async () => {
+        if (!camera || !camera.current) {
+            throw new Error("camera not referenced yet");
+        }
+        const data = await camera.current.takePictureAsync(captureOpts);
+        onPicTaken(data);
     }
-    const data = await camera.current.takePictureAsync(captureOpts);
-    onPicTaken(data);
-  }
+    const [type, setType] = useState('back');
+    const toggleFacing = () => setType(type === 'back' ? 'front' : 'back');
 
-  return <>
-    <Flex>
-      <Flex
-        as={RNCamera}
-        ref={camera}
-        type='back'
-        captureAudio={false}
-      />
-      <Content>
-        <CameraCorners />
-        <Fill />
-        <TriggerWrap inset={safeAreaInsets.bottom}>
-          <Trigger onPress={takePictureAsync} />
-        </TriggerWrap>
-      </Content>
-    </Flex>
-  </>
+    return (
+        <Flex>
+            <Flex
+                as={RNCamera}
+                ref={camera}
+                type={type}
+                captureAudio={false}
+            />
+            <Content>
+                <Header>
+                    <FlipButton onPress={toggleFacing}>
+                        <FlipText> FLIP </FlipText>
+                    </FlipButton>
+                </Header>
+                <CameraCorners />
+                <Fill />
+                <TriggerWrap inset={safeAreaInsets.bottom}>
+                    <Trigger onPress={takePictureAsync} />
+                </TriggerWrap>
+            </Content>
+        </Flex>
+    );
 }
